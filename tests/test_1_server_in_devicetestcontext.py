deed476c20d9b0aa97462291075b5dc112c0df1f
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2019-01-23 09:37:57 +0000

    Fix lint warnings Remove unnecessary containers from docker-compose

--- tests/test_1_server_in_devicetestcontext.py
+++ tests/test_1_server_in_devicetestcontext.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+Some simple unit tests of the PowerSupply device, exercising the device from
+the same host as the tests by using a DeviceTestContext.
+"""
 from tango import DevState
 from tango.test_utils import DeviceTestContext
 
@@ -6,29 +10,33 @@ from powersupply.powersupply import PowerSupply
 
 
 def test_init():
+    """Test device goes into STANDBY when initialised"""
     with DeviceTestContext(PowerSupply, process=True) as proxy:
         proxy.Init()
         assert proxy.state() == DevState.STANDBY
 
 
 def test_turn_on():
+    """Test device turns on when requested"""
     with DeviceTestContext(PowerSupply, process=True) as proxy:
         proxy.Init()
         assert proxy.state() != DevState.ON
         proxy.current = 5.0
-        proxy.TurnOn()
+        proxy.turn_on()
         assert proxy.state() == DevState.ON
 
 
 def test_turn_off():
+    """Test device turns off when requested"""
     with DeviceTestContext(PowerSupply, process=True) as proxy:
         proxy.Init()
         assert proxy.state() != DevState.OFF
-        proxy.TurnOff()
+        proxy.turn_off()
         assert proxy.state() == DevState.OFF
 
 
 def test_current_is_zero_at_init():
+    """Test device sets current to 0 when initialised"""
     with DeviceTestContext(PowerSupply, process=True) as proxy:
         proxy.Init()
         proxy.current = 5
@@ -38,6 +46,7 @@ def test_current_is_zero_at_init():
 
 
 def test_set_current():
+    """Test device sets current on request"""
     with DeviceTestContext(PowerSupply, process=True) as proxy:
         proxy.current = 5.0
         assert proxy.current == 5.0
