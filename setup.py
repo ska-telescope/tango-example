5e94601e5316a69b0e37ceb581c63f051a94cc28
Matteo Canzari <>
2019-10-17 16:43:38 +0200

    solved failed test

--- setup.py
+++ setup.py
@@ -15,52 +15,7 @@ setup(
     author_email='stewart.williams@stfc.ac.uk',
     url='https://github.com/ska-telescope/tango-example',
     packages=[
-        'powersupply', 'Motor', 'EventReceiver'
-    ],
-    include_package_data=True,
-    license="BSD license",
-    zip_safe=False,
-    keywords='ska_python_skeleton',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    install_requires=['pytango'],  # FIXME: add your package's dependencies to this list
-    setup_requires=[
-        # dependency for `python setup.py test`
-        'pytest-runner',
-        # dependencies for `python setup.py build_sphinx`
-        'sphinx',
-        'recommonmark'
-    ],
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-        'pytest-json-report',
-        'pycodestyle',
-    ],
-    extras_require={
-        'dev':  ['prospector[with_pyroma]', 'yapf', 'isort']
-    }
-)
-
-setup(
-    name='webjivetestdevice',
-    version='0.0.0',
-    description="",
-    long_description=readme + '\n\n',
-    author="Matteo Canzari",
-    author_email='matteo.canzari@inaf.it',
-    url='https://github.com/ska-telescope/tango-example',
-    packages=[
-        'WebjiveTestDevice'
+        'powersupply', 'Motor', 'EventReceiver', 'WebjiveTestDevice'
     ],
     include_package_data=True,
     license="BSD license",
