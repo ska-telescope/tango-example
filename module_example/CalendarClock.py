a0030eaf7251afdd704c3835a5985b260c97e6d9
SKAJohanVenter <aventer@ska.ac.za>
2020-02-14 11:38:51 +0200

    Fixed pylint errors

--- module_example/CalendarClock.py
+++ module_example/CalendarClock.py
@@ -1,3 +1,4 @@
+# pylint: disable=C0103
 """ This module illustates the humble object principal whereby the business logic is
 seperated from the external interfaces.
 
@@ -17,11 +18,9 @@ class TestCalendarClock
 """
 
 from enum import IntEnum
-from datetime import datetime
 
-from tango import AttrQuality, AttrWriteType, DispLevel, DevState, DevFailed, DeviceProxy
-from tango.server import attribute, command, device_property, run
-from tango import Database, DbDevInfo, DbServerInfo
+from tango import AttrWriteType, DevState
+from tango.server import attribute, command, run
 
 from skabase.SKABaseDevice.SKABaseDevice import SKABaseDevice
 
@@ -35,20 +34,23 @@ CURRENT_SECOND = 6
 
 
 class DateStyle(IntEnum):
+    """Style of the date"""
+
     BRITISH = 0  # DevEnum's must start at 0
     AMERICAN = 1  # and increment by 1
 
 
-class CalendarClockModel:
+class CalendarClockModel:  # pylint: disable=R0902
     """This model illustrates the humble object concept whereby the business logic is
     seperated from external component interfaces.
     """
 
-    months = (31,28,31,30,31,30,31,31,30,31,30,31)
+    months = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
     _date_style = DateStyle.BRITISH
 
     @property
     def date_style(self):
+        """date-style property"""
         return self._date_style
 
     @staticmethod
@@ -59,14 +61,21 @@ class CalendarClockModel:
         """
         if not year % 4 == 0:
             return False
-        elif not year % 100 == 0:
+        if not year % 100 == 0:
             return True
-        elif not year % 400 == 0:
+        if not year % 400 == 0:
             return False
-        else:
-            return True
+        return True
 
-    def __init__(self, day, month, year, hour, minute, second):
+    def __init__(self, day, month, year, hour, minute, second):  # pylint: disable=R0913
+        """Init the model"""
+        self._year = None
+        self._month = None
+        self._day = None
+        self._hour = None
+        self._minute = None
+        self._second = None
+
         self.set_calendar(day, month, year)
         self.set_clock(hour, minute, second)
 
@@ -76,7 +85,7 @@ class CalendarClockModel:
         a four digit year number
         """
 
-        if type(day) == int and type(month) == int and type(year) == int:
+        if isinstance(day, int) and isinstance(month, int) and isinstance(year, int):
             self._day = day
             self._month = month
             self._year = year
@@ -92,15 +101,15 @@ class CalendarClockModel:
         0 <= s < 60
         """
 
-        if type(hour) == int and 0 <= hour and hour < 24:
+        if isinstance(hour, int) and 0 <= hour < 24:
             self._hour = hour
         else:
             raise TypeError("Hour have to be integers between 0 and 23!")
-        if type(minute) == int and 0 <= minute and minute < 60:
+        if isinstance(minute, int) and 0 <= minute < 60:
             self._minute = minute
         else:
             raise TypeError("Minute have to be integers between 0 and 59!")
-        if type(second) == int and 0 <= second and second < 60:
+        if isinstance(second, int) and 0 <= second < 60:
             self._second = second
         else:
             raise TypeError("Second have to be integers between 0 and 59!")
@@ -143,7 +152,7 @@ class CalendarClockModel:
         if self._month == 2 and self.leapyear(self._year):
             max_days += 1
         if self._day == max_days:
-            self._day= 1
+            self._day = 1
             if self._month == 12:
                 self._month = 1
                 self._year += 1
@@ -165,27 +174,31 @@ class CalendarClockModel:
             self.set_device_state(DevState.ON)
 
     def swith_off(self):
+        """Switch the device off"""
         if self.get_device_state() != DevState.OFF:
             self.set_device_state(DevState.OFF)
 
     def __str__(self):
+        """String representation of the model"""
         datetime_style = "{0:02d}/{1:02d}/{2:04d} {3:02d}:{4:02d}:{5:02d}"
         if self._date_style == DateStyle.BRITISH:
             return datetime_style.format(
                 self._day, self._month, self._year,
                 self._hour, self._minute, self._second)
-        elif self._date_style == DateStyle.AMERICAN:
-            return datetime_style.format(
-                self._month, self._day, self._year,
-                self._hour, self._minute, self._second)
+        return datetime_style.format(
+            self._month, self._day, self._year,
+            self._hour, self._minute, self._second)
 
 
 class CalendarClockDevice(SKABaseDevice):
+    """The Tango device CalendarClockDevice"""
 
     def __init__(self, *args, **kwargs):
+        """Init the class"""
         SKABaseDevice.__init__(self, *args, **kwargs)
 
     def init_device(self):
+        """Init the device"""
         SKABaseDevice.init_device(self)
         self.model = CalendarClockModel(CURRENT_DAY,
                                         CURRENT_MONTH,
@@ -193,63 +206,77 @@ class CalendarClockDevice(SKABaseDevice):
                                         CURRENT_HOUR,
                                         CURRENT_MINUTE,
                                         CURRENT_SECOND)
-        self.model.get_device_state = self.get_state
-        self.model.set_device_state = self.set_state
+        self.model.get_device_state = self.get_state # pylint: disable=W0201
+        self.model.set_device_state = self.set_state # pylint: disable=W0201
         self.set_state(DevState.UNKNOWN)
 
     @attribute(dtype=DateStyle, access=AttrWriteType.READ_WRITE)
     def date_style(self):
+        """date_style attribute"""
         return self.model.date_style
 
     def write_date_style(self, value):
-        self.model._date_style = value
+        """Set the date_style"""
+        self.model._date_style = value  # pylint: disable=W0212
 
     @attribute
     def day(self):
-        return self.model._day
+        """The day of the month"""
+        return self.model._day  # pylint: disable=W0212
 
     @attribute
     def month(self):
-        return self.model._month
+        """The month in the year"""
+        return self.model._month  # pylint: disable=W0212
 
     @attribute
     def year(self):
-        return self.model._year
+        """The year"""
+        return self.model._year  # pylint: disable=W0212
 
     @attribute
     def hour(self):
-        return self.model._hour
+        """The hour in the day"""
+        return self.model._hour  # pylint: disable=W0212
 
     @attribute
     def minute(self):
-        return self.model._minute
+        """The minute in the hour"""
+        return self.model._minute  # pylint: disable=W0212
 
     @attribute
     def second(self):
-        return self.model._second
+        """The second in the minute"""
+        return self.model._second  # pylint: disable=W0212
 
     @command
-    def Advance(self):
+    def Advance(self): # pylint: disable=C0103
+        """Advande the clock 1 day"""
         self.model.advance()
 
     @command
-    def Tick(self):
+    def Tick(self): # pylint: disable=C0103
+        """Advande the clock 1 second"""
         self.model.tick()
 
     @command
-    def SwitchOn(self):
+    def SwitchOn(self): # pylint: disable=C0103
+        """Swith the device on"""
         self.model.swith_on()
 
     @command
-    def SwitchOff(self):
+    def SwitchOff(self): # pylint: disable=C0103
+        """Swith the device off"""
         self.model.swith_off()
 
     @command(dtype_out=str)
-    def GetFormattedTime(self):
+    def GetFormattedTime(self): # pylint: disable=C0103
+        """Get the formatted string of the datetime"""
         return str(self.model)
 
 
 def main(args=None, **kwargs):
+    """Run CalendarClockDevice"""
     return run((CalendarClockDevice,), args=args, **kwargs)
 
 if __name__ == '__main__':
