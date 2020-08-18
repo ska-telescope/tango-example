f925597cfa93c9a154dc1ae34db450405d3f48a4
kmadisa <katleho.madisa47@gmail.com>
2020-08-18 17:51:11 +0200

    Update test_power_supply_consumer module.

--- pact-spike/non-tango-rest-example/test_power_supply_consumer.py
+++ pact-spike/non-tango-rest-example/test_power_supply_consumer.py
@@ -1,30 +1,30 @@
 import pytest
 from pactman import Consumer, Provider, Term
-from consumer import get_calendar_date
+from consumer import get_current
 from mock_dev_proxy import TangoPact
 
 @pytest.fixture
 def pact():
-    pact = Consumer('Consumer', TangoPact).has_pact_with(Provider('CalendarClock'))
+    pact = Consumer('Consumer', TangoPact).has_pact_with(Provider('PowerSupply'))
     pact.start_mocking()
     yield pact
     pact.stop_mocking()
 
-def test_calendar_date(pact):
-    expected = Term(r'\d+/\d+/\d+', '03/02/2001')
+def test_voltage(pact):
+    expected = 0.0
     # a read_attribute request returns a DeviceAttribute object with values for
     # of its attributes. At the moment we're retrieving only the name, value, quality
     # and timestamp values from the object just like what the tango rest api server returns
 
     (pact
-     .given('calendarclockdevice is running')
-     .upon_receiving('a read request for calendar_date attribute')
+     .given('powersupply is running')
+     .upon_receiving('a read request for the current attribute')
      .with_request('read_attribute')
      .will_respond_with(expected)) # reply can be tango obj, exception obj, none, int, ...
 
     with pact:
-        result = get_calendar_date("test/calendarclockdevice/1")
-        assert result == '03/02/2001'
+        result = get_current("test/power_supply/1")
+        assert result == 0.0
 
 # def test_tick(pact):
 #     command_name = "tick"
