8d3c30a550f989c066c714fc49f2b306f98757de
Katleho Madisa <katleho.madisa47@gmail.com>
2021-05-20 00:13:45 +0200

    Create one attribute and one command

--- src/other_examples/test_device2.py
+++ src/other_examples/test_device2.py
@@ -11,73 +11,19 @@ class TestDevice2(Device):
 
     def init_device(self):
         super().init_device()
-        self.__polled_attr_1 = random.randint(0, 150)
-        self.__polled_attr_2 = random.randint(0, 150)
-        self.__polled_attr_3 = random.randint(0, 150)
-        self.__polled_attr_4 = random.randint(0, 150)
-        self.__polled_attr_5 = random.randint(0, 150)
-
-    # -----------------
-    # Polled attributes
-    # -----------------
-    @attribute(
-        dtype="int",
-        polling_period=200,
-        rel_change="0.5",
-        abs_change="1",
-    )
-    def polled_attr_1(self):
-        return int(self.__polled_attr_1)
+        self._attr_1 = random.randint(0, 150)
 
     @attribute(
         dtype="int",
-        polling_period=400,
-        rel_change="1",
-        abs_change="1",
+        polling_period=500
     )
-    def polled_attr_2(self):
-        return int(self.__polled_attr_2)
-
-    @attribute(
-        dtype="int",
-        polling_period=600,
-        rel_change="1.5",
-        abs_change="1",
-    )
-    def polled_attr_3(self):
-        return int(self.__polled_attr_3)
-
-    @attribute(
-        dtype="int",
-        polling_period=800,
-        rel_change="1.7",
-        abs_change="1",
-    )
-    def polled_attr_4(self):
-        return int(self.__polled_attr_4)
-
-    @attribute(
-        dtype="int",
-        polling_period=1000,
-        rel_change="1.7",
-        abs_change="1",
-    )
-    def polled_attr_5(self):
-        return int(self.__polled_attr_5)
-
-    # -------
-    # Command
-    # --------
+    def attr_1(self):
+        return int(self._attr_1)
 
     @command(
-        dtype_in=None,
-        doc_in="",
-        dtype_out="str",
-        doc_out="Some dummy message.",
     )
     def ExecuteWithADelay(self):
         time.sleep(2)
-        return "ExecuteWithADelay command finished executing after 2 seconds."
 
 
 if __name__ == "__main__":
