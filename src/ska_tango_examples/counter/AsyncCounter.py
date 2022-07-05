a7eec6cb2f7a76ba7a44bcfcbe9526339dfb0a2b
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2022-07-05 15:18:44 +0200

    ST-1297 fix async counter
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- src/ska_tango_examples/counter/AsyncCounter.py
+++ src/ska_tango_examples/counter/AsyncCounter.py
@@ -17,11 +17,9 @@ A simple counter:
 - reset
 """
 
-from tango import GreenMode
-from tango.server import Device, run
-
 # PyTango imports
-from ska_tango_examples.counter.Counter import Counter
+from tango import AttrWriteType, DebugIt, GreenMode
+from tango.server import Device, attribute, command, run
 
 # Additional import
 # PROTECTED REGION ID(Counter.additionnal_import) ENABLED START #
@@ -31,7 +29,7 @@ from ska_tango_examples.counter.Counter import Counter
 __all__ = ["AsyncCounter", "main"]
 
 
-class AsyncCounter(Counter):
+class AsyncCounter(Device):
     """
     This Device demonstrate the use of the TANGO event mechanism
     to send change events to clients.
@@ -42,6 +40,25 @@ class AsyncCounter(Counter):
     # PROTECTED REGION ID(Counter.class_variable) ENABLED START #
     green_mode = GreenMode.Asyncio
     # PROTECTED REGION END #    //  Counter.class_variable
+
+    # ----------
+    # Attributes
+    # ----------
+
+    value = attribute(
+        dtype="DevShort",
+    )
+
+    fire_event_at = attribute(
+        dtype="DevShort",
+        access=AttrWriteType.READ_WRITE,
+    )
+
+    polled_value = attribute(
+        dtype="DevShort",
+        period=100,
+        abs_change=1,
+    )
 
     # ---------------
     # General methods
@@ -57,6 +74,107 @@ class AsyncCounter(Counter):
         self.set_change_event("polled_value", True, True)
         # PROTECTED REGION END #    //  Counter.init_device
 
+    def always_executed_hook(self):
+        """Method always executed before any TANGO command is executed."""
+        # PROTECTED REGION ID(Counter.always_executed_hook) ENABLED START #
+        # PROTECTED REGION END #    //  Counter.always_executed_hook
+
+    def delete_device(self):
+        """Hook to delete resources allocated in init_device.
+
+        This method allows for any memory or other resources allocated in the
+        init_device method to be released.  This method is called by the device
+        destructor and by the device Init command.
+        """
+        # PROTECTED REGION ID(Counter.delete_device) ENABLED START #
+        # PROTECTED REGION END #    //  Counter.delete_device
+
+    # ------------------
+    # Attributes methods
+    # ------------------
+
+    def read_value(self):
+        # PROTECTED REGION ID(Counter.value_read) ENABLED START #
+        """Return the value attribute."""
+        return self._value
+        # PROTECTED REGION END #    //  Counter.value_read
+
+    def read_fire_event_at(self):
+        # PROTECTED REGION ID(Counter.fire_event_at_read) ENABLED START #
+        """Return the fire_event_at attribute."""
+        return self._fire_event_at
+        # PROTECTED REGION END #    //  Counter.fire_event_at_read
+
+    def write_fire_event_at(self, value):
+        # PROTECTED REGION ID(Counter.fire_event_at_write) ENABLED START #
+        """Set the fire_event_at attribute."""
+        self._fire_event_at = value
+        # PROTECTED REGION END #    //  Counter.fire_event_at_write
+
+    def read_polled_value(self):
+        # PROTECTED REGION ID(Counter.polled_value_read) ENABLED START #
+        """Return the polled_value attribute."""
+        return self._value
+        # PROTECTED REGION END #    //  Counter.polled_value_read
+
+    # --------
+    # Commands
+    # --------
+
+    @command(
+        dtype_out="DevShort",
+    )
+    @DebugIt()
+    def increment(self):
+        # PROTECTED REGION ID(Counter.increment) ENABLED START #
+        """
+        Increment the value of the counter by 1
+
+        :return:'DevShort'
+        """
+        self._value += 1
+        if self._value == self._fire_event_at:
+            self.push_change_event("value", self._value)
+        return self._value
+        # PROTECTED REGION END #    //  Counter.increment
+
+    @command(
+        dtype_out="DevShort",
+    )
+    @DebugIt()
+    def decrement(self):
+        # PROTECTED REGION ID(Counter.decrement) ENABLED START #
+        """
+        Decrement the value of the counter by 1
+
+        :return:'DevShort'
+        """
+        self._value -= 1
+        if self._value == self._fire_event_at:
+            self.push_change_event("value", self._value)
+        return self._value
+        # PROTECTED REGION END #    //  Counter.decrement
+
+    @command(
+        dtype_in="DevShort",
+        dtype_out="DevShort",
+    )
+    @DebugIt()
+    def CounterReset(self, argin):
+        # PROTECTED REGION ID(Counter.CounterReset) ENABLED START #
+        """
+        Reset the counter to the input parameter
+
+        :param argin: 'DevShort'
+
+        :return:'DevShort'
+        """
+        self._value = argin
+        if self._value == self._fire_event_at:
+            self.push_change_event("value", self._value)
+        return self._value
+        # PROTECTED REGION END #    //  Counter.CounterReset
+
 
 # ----------
 # Run server
@@ -66,7 +184,6 @@ class AsyncCounter(Counter):
 def main(args=None, **kwargs):
     """Main function of the Counter module."""
     # PROTECTED REGION ID(Counter.main) ENABLED START #
-    kwargs.setdefault("green_mode", GreenMode.Asyncio)
     return run((AsyncCounter,), args=args, **kwargs)
     # PROTECTED REGION END #    //  Counter.main
 
