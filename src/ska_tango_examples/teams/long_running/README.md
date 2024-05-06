524bcf7db3387068acc319ed0f4f8fa5f939c24a
Jarrett Engelbrecht <jarrett@vivosa.co.za>
2024-05-06 13:53:00 +0200

    WOM-242 Restore long running commands examples removed with ST-1799
    This partially reverts commit 551abebb.

---
+++ src/ska_tango_examples/teams/long_running/README.md
@@ -0,0 +1,62 @@
+# Long Running Command Sample Devices
+
+## Deprecation notice
+
+This example is very old and not used anymore. Please do not take it as reference (in future it could be removed).
+
+## Introduction
+
+This module includes various devices to illustrate how LRCs are used.
+
+Two ways to keep track of task completion are also illustrated:
+- `On`/`Off` commands waiting on state changes of subservient devices to determine command completion.
+- `Scan` command using callbacks on attribute `longRunningCommandResult`of subservient devices to determine command completion.
+
+The examples include the usage of a component manager to encapsulate business logic.
+
+## Device layout
+
+                         Controller (test/lrccontroller/1)
+                            |                         |
+        Station(test/lrcstation/1)                Station(test/lrcstation/2)
+                |           |                         |                  |
+    Tile(test/lrctile/1)  Tile(test/lrctile/2)   Tile(test/lrctile/3)  Tile(test/lrctile/4)
+
+On/Off/Scan  commands propagate down from the Controller to Tile.
+
+## Command flow
+
+### `On` ( and `Off`)
+
+  - #### _Controller_
+    - Controller receives `On`
+    - `On` is executed on Stations
+    - Controller waits for Station state to change to `On`
+      - #### _Station_
+        - Station receives `On`
+        - `On` is executed on Tiles
+        - Station waits for Tile state to change to `On`
+          - #### _Tile_
+            - Tile goes to `On` state
+        - Station goes to `On` state
+    - Controller goes to `On` state
+
+### `Scan`
+
+  - #### _Controller_
+    - Controller receives `Scan`
+    - Component Manager set to `scanning`
+    - Controller subscribes to change event on Station attribute `longRunningCommandResult`
+    - `Scan` is executed on Stations
+      - #### _Station_
+        - Station receives `Scan`
+        - Component Manager set to `scanning`
+        - Station subscribes to change event on Tile attribute `longRunningCommandResult`
+        - `Scan` is executed on Tiles
+          - #### _Tile_
+            - Tile receives `Scan`
+            - `Scan` is completed, change event on `longRunningCommandResult`
+        - Station receives change event on Tile result
+        - Component Manager unsets `scanning`
+    - Controller receives change event on Station result
+    - Component Manager unsets `scanning`
---
+++ src/ska_tango_examples/teams/long_running/Station.py
@@ -0,0 +1,201 @@
+# pylint: disable=abstract-method
+import time
+
+import tango
+from ska_tango_base import SKABaseDevice
+from ska_tango_base.commands import ResultCode, SlowCommand
+from ska_tango_base.executor import TaskExecutorComponentManager
+from tango.server import command, device_property, run
+
+from ska_tango_examples.teams.long_running.utils import (
+    LongRunningDeviceInterface,
+)
+
+
+class StationComponentManager(TaskExecutorComponentManager):
+    def __init__(
+        self,
+        max_queue_size,
+        num_workers,
+        logger=None,
+        push_change_event=None,
+        tiles=(),
+    ):
+        self.max_queue_size = max_queue_size
+        self.num_workers = num_workers
+        self.logger = logger
+        self.push_change_event = push_change_event
+        self.tiles = tiles
+        self.scanning = False
+        super().__init__(None)
+
+    def get_tile_proxies(self):
+        return [tango.DeviceProxy(tile) for tile in self.tiles]
+
+    def on(self):
+        for proxy in self.get_tile_proxies():
+            proxy.On()
+
+    def off(self):
+        for proxy in self.get_tile_proxies():
+            proxy.Off()
+
+    def _wait_for_state(self, state: tango.DevState, timeout: int = 5):
+        while timeout > 0:
+            if all(
+                proxy.State() == state for proxy in self.get_tile_proxies()
+            ):
+                return True
+            timeout -= 1
+            time.sleep(1)
+        return False
+
+    def wait_for_on(self, timeout=5):
+        return self._wait_for_state(tango.DevState.ON, timeout=timeout)
+
+    def wait_for_off(self, timeout=5):
+        return self._wait_for_state(tango.DevState.OFF, timeout=timeout)
+
+    def _scan_complete(self, command_name, command_ids):
+        assert command_name == "Scan"
+        assert len(command_ids) == 2
+        self.scanning = False
+
+    def scan(self):
+        self.logger.info("Scan started")
+        self.scanning = True
+        tiles_interface = LongRunningDeviceInterface(
+            tango_devices=self.tiles, logger=self.logger
+        )
+        tiles_interface.execute_long_running_command(
+            "Scan", on_completion_callback=self._scan_complete
+        )
+        self.logger.info("Scan done")
+
+    def wait_for_scan_completion(self, timeout=5):
+        while timeout > 0:
+            if not self.scanning:
+                return True
+            timeout -= 1
+            time.sleep(1)
+        return False
+
+
+class Station(SKABaseDevice):
+
+    tiles = device_property(
+        dtype=[
+            str,
+        ],
+        mandatory=False,
+        default_value=["test/lrctile/1", "test/lrctile/2"],
+    )
+
+    def init_command_objects(self):
+        self._command_objects = {}
+        self.register_command_object(
+            "On", self.OnCommand(target=self, logger=self.logger)
+        )
+        self.register_command_object(
+            "Off", self.OffCommand(target=self, logger=self.logger)
+        )
+        self.register_command_object(
+            "Scan",
+            self.ScanCommand(
+                target=self.component_manager, logger=self.logger
+            ),
+        )
+
+    def create_component_manager(self):
+        return StationComponentManager(
+            max_queue_size=2,
+            num_workers=2,
+            logger=self.logger,
+            push_change_event=self.push_change_event,
+            tiles=self.tiles,
+        )
+
+    class OnCommand(SlowCommand):
+        def __init__(self, target, logger=None):
+            """"""
+            self.target = target
+            super().__init__(callback=None, logger=logger)
+
+        def do(self):
+            """"""
+            device = self.target
+            device.component_manager.on()
+            if device.component_manager.wait_for_on(timeout=5):
+                device.set_state(tango.DevState.ON)
+                return ResultCode.OK, "On completed"
+            else:
+                raise RuntimeError("Not all tiles turned on")
+
+    @command(
+        dtype_in=None,
+        dtype_out="DevVarLongStringArray",
+        doc_out="([Command ResultCode], [Unique ID of the command])",
+    )
+    def On(self):
+        handler = self.get_command_object("On")
+        unique_id, return_code = self.component_manager.enqueue(handler)
+        return [return_code], [unique_id]
+
+    class OffCommand(SlowCommand):
+        def __init__(self, target, logger=None):
+            """"""
+            self.target = target
+            super().__init__(callback=None, logger=logger)
+
+        def do(self):
+            """"""
+            device = self.target
+            device.component_manager.off()
+            if device.component_manager.wait_for_off(timeout=10):
+                device.set_state(tango.DevState.OFF)
+                return ResultCode.OK, "Station Off completed"
+            else:
+                raise RuntimeError("Not all stations turned off")
+
+    @command(
+        dtype_in=None,
+        dtype_out="DevVarLongStringArray",
+        doc_out="([Command ResultCode], [Unique ID of the command])",
+    )
+    def Off(self):
+        handler = self.get_command_object("Off")
+        unique_id, return_code = self.component_manager.enqueue(handler)
+        return [return_code], [unique_id]
+
+    class ScanCommand(SlowCommand):
+        def __init__(self, target, logger=None):
+            """"""
+            self.target = target
+            super().__init__(callback=None, logger=logger)
+
+        def do(self):
+            """"""
+            component_manager = self.target
+            component_manager.scan()
+            if component_manager.wait_for_scan_completion(timeout=10):
+                return ResultCode.OK, "Station Scan completed"
+            else:
+                raise RuntimeError("Scan did not complete in time")
+
+    @command(
+        dtype_in=None,
+        dtype_out="DevVarLongStringArray",
+        doc_out="([Command ResultCode], [Unique ID of the command])",
+    )
+    def Scan(self):
+        handler = self.get_command_object("Scan")
+        unique_id, return_code = self.component_manager.enqueue(handler)
+        return [return_code], [unique_id]
+
+
+def main(args=None, **kwargs):
+    return run((Station,), args=args, **kwargs)
+
+
+if __name__ == "__main__":
+    main()
