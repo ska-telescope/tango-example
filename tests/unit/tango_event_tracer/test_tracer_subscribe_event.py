58b4dbd3b7e6d6ecbfcc9e59007020f2ff284452
Emanuele Lena <emanuele.lena@designcoaching.net>
2024-06-05 15:08:30 +0200

    OMCPT-52: try what happen when you subscribe late to events

--- tests/unit/tango_event_tracer/test_tracer_subscribe_event.py
+++ tests/unit/tango_event_tracer/test_tracer_subscribe_event.py
@@ -185,3 +185,81 @@ def test_tracer_when_device_not_found_raises_exception(tango_context):
 
     with pytest.raises(DevFailed):
         sut.subscribe_to_device("test/pollingdemo/100", "subscrib_attr")
+
+
+def test_tracer_late_subscription_catches_last_event(tango_context):
+    """A late subscription catches just the last event.
+
+    When you subscribe to a device attribute, after a sequence of events
+    have already been triggered, the subscription should only catch the
+    last event.
+    """
+    proxy = tango_context.get_device("test/pollingdemo/1")
+
+    proxy.increment_subscrib()
+    proxy.increment_subscrib()
+    proxy.increment_subscrib()
+
+    time.sleep(1)
+
+    sut = TangoEventTracer()
+    sut.subscribe_to_device("test/pollingdemo/1", "subscrib_attr")
+
+    query_result = sut.query_events(
+        lambda e: e.device_name == "test/pollingdemo/1"
+        and e.attribute_name == "subscrib_attr"
+    )
+
+    assert_that(query_result).described_as(
+        "Expected exactly four events to be captured, but got "
+        f"{'more' if len(query_result) > 1 else 'none'} "
+        f"(tot: {len(query_result)} instead of 1)"
+    ).is_length(1)
+
+    assert_that(query_result[0].attribute_value).is_equal_to(3)
+
+    # those assertions would fail!
+    # assert_that(query_result[0].attribute_value).is_equal_to(0)
+    # assert_that(query_result[1].attribute_value).is_equal_to(1)
+    # assert_that(query_result[2].attribute_value).is_equal_to(2)
+    # assert_that(query_result[3].attribute_value).is_equal_to(3)
+
+
+@pytest.mark.xfail(
+    reason="Right now, multiple subscriptions duplicate the events"
+    "TODO: discuss if this is the desired behavior or not and"
+    "eventually fix the code to avoid this."
+)
+def test_tracer_multiple_subscription_doesnt_duplicate_events(tango_context):
+    """Multiple subscriptions don't duplicate events.
+
+    When you subscribe to a device attribute multiple times, the events
+    should not be duplicated.
+    """
+    proxy = tango_context.get_device("test/pollingdemo/1")
+
+    sut = TangoEventTracer()
+    sut.subscribe_to_device("test/pollingdemo/1", "subscrib_attr")
+    sut.subscribe_to_device("test/pollingdemo/1", "subscrib_attr")
+
+    proxy.increment_subscrib()
+    proxy.increment_subscrib()
+    proxy.increment_subscrib()
+
+    time.sleep(1)
+
+    query_result = sut.query_events(
+        lambda e: e.device_name == "test/pollingdemo/1"
+        and e.attribute_name == "subscrib_attr"
+    )
+
+    assert_that(query_result).described_as(
+        "Expected exactly four event to be captured, but got "
+        f"{'more' if len(query_result) > 4 else 'none'} "
+        f"(tot: {len(query_result)} instead of 4)"
+    ).is_length(4)
+
+    assert_that(query_result[0].attribute_value).is_equal_to(0)
+    assert_that(query_result[1].attribute_value).is_equal_to(1)
+    assert_that(query_result[2].attribute_value).is_equal_to(2)
+    assert_that(query_result[3].attribute_value).is_equal_to(3)
